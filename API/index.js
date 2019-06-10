ae4ba736853cd116b52ebf4e0e339e472819005b
sempsmahad <mhdsemps@gmail.com>
2019-06-10 11:58:31 +0200

    [update ] updated the project structure to comply the expanding architectures

--- API/index.js
+++ API/index.js
@@ -1,1 +1,17 @@
-console.log('hello' + 'my' + 'friend');
+import express from 'express';
+import bodyParser from 'body-parser';
+
+const app = express();
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: false }));
+
+
+// when a random route is inputed
+app.get('*', (req, res) => res.status(200).send({
+  message: 'Welcome to this API.'
+}));
+const port = process.env.PORT || 8000;
+app.listen(port, () => {
+  console.log(`Server is running on PORT ${port}`);
+});
+export default app;
